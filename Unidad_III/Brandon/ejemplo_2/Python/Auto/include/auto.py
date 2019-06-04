f627d3ffd810caa6bf380dfcf3049e171b592e11
brandonA <brandiu1997@hotmail.com>
2019-06-04 09:52:02 -0500

    Terminado de python

--- Unidad_III/Brandon/ejemplo_2/Python/Auto/include/auto.py
+++ Unidad_III/Brandon/ejemplo_2/Python/Auto/include/auto.py
@@ -3,17 +3,21 @@ class Auto:
         self.marca=pMarca
         self.modelo=pModelo
         self.year=pYear
-        self.kilo=pKilo
+        self.kilo=int(pKilo)
         self.color=pColor
+    
     def Pintar(self,pColor):
         self.color=pColor
         print("El Nuevo color es: "+str(self.color))
+    
     def Correr(self,pKilo):
-        self.kilo=(pKilo+self.kilo)
+        self.kilo=int(pKilo)+self.kilo
         print("El auto viajo: "+str(self.kilo))
+    
     def Modificar(self,pModelo):
         self.modelo=pModelo
         print("El Nuevo modelo es: "+str(self.modelo))
+    
     def Checar(self):
         print("La marca es: "+str(self.marca))
         print("El modelo es: "+str(self.modelo))
--- Unidad_III/Brandon/ejemplo_2/Python/Auto/src/agencia.py
+++ Unidad_III/Brandon/ejemplo_2/Python/Auto/src/agencia.py
@@ -4,8 +4,24 @@ from auto import Auto
 
 auto1 = Auto()
 
-pMarca=input("Ingrese la marca")
-pModelo=input("Ingrese el modelo")
-pYear=input("Ingrese el año")
-pKilo=input("Ingrese el Kilometraje")
-pColor=input("Ingrese el color")
+pMarca=input("Ingrese la marca:  ")
+pModelo=input("Ingrese el modelo:  ")
+pYear=input("Ingrese el año:  ")
+pKilo=input("Ingrese el Kilometraje:  ")
+pColor=input("Ingrese el color:  ")
+
+auto2 = Auto(pMarca,pModelo,pYear,pKilo,pColor)
+
+auto1.Checar()
+auto2.Checar()
+
+pColor=input("Ingrese el nuevo color: ")
+auto2.Pintar(pColor)
+
+pKilo=input("Cuanto corrio el auto? ")
+auto2.Correr(pKilo)
+
+pModelo=input("Cual es el nuevo modelo: ")
+auto2.Modificar(pModelo)
+
+auto2.Checar()
